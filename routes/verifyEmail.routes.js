3e9526b7b5cb1c540b6085cd880f3a0d6e5addb7
MajidShahDev <mail.syedmajid@gmail.com>
2026-02-11 19:40:26 +0500

    rate limiting added

--- routes/verifyEmail.routes.js
+++ routes/verifyEmail.routes.js
@@ -1,3 +1,4 @@
+
 // routes/verifyEmail.routes.js
 import express from "express";
 import {
@@ -5,13 +6,14 @@ import {
   handleVerifyEmail,
   handleResendVerificationEmail,
 } from "../controllers/verifyEmail.controller.js";
+import { generalAuthLimiter } from "../middlewares/rateLimiter.js";
 
 
 const router = express.Router();
 
 // send verification email (logged-in user)
-router.post("/verify-email", handleSendVerificationEmail);
-router.post("/resend-verification", handleResendVerificationEmail);
+router.post("/verify-email", generalAuthLimiter, handleSendVerificationEmail);
+router.post("/resend-verification", generalAuthLimiter, handleResendVerificationEmail);
 
 // verify email via token
 router.get("/verify-email/:token", handleVerifyEmail);
