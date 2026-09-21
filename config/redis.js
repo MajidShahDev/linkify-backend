01c44405f43b3cd9803666eb4eec225b4961c154
MajidShahDev <mail.syedmajid@gmail.com>
2026-09-21 15:14:00 +0500

    refactor(error): handle common redis errors centrally

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,14 @@
 import Redis from "ioredis";
+import { appLogger } from "./logger.js";
 
 const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
 redis.on('ready', () => console.log('Redis: Ready'));
-redis.on('error', (e) => console.error('Redis Error', e));
+redis.on("error", (err) => {
+  appLogger.error({
+    type: "redis-error",
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 export default redis;
